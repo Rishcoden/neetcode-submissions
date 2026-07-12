49c604e7541952f7e0c544062d14003b7634e4dc
Kaarthik Rishaanth <163330996+Rishcoden@users.noreply.github.com>
2026-07-12 21:19:40 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+
+   boolean check(int idx,int arr[],int visited[]){
+    if(idx==arr.length-1){
+        return true;
+    }
+    int val = arr[idx];
+     if(val==0){
+        return false;
+     }
+    for(int i=idx+1;i<=idx+val;i++){
+       if(visited[i]==1){
+           return true;
+       }
+       boolean ans = check(i,arr,visited);
+       if(ans){
+        visited[i] = 1;
+        return true;
+       }
+    }
+    
+    return false;
+   }
+
+    public boolean canJump(int[] nums) {
+        int visited[] = new int[nums.length];
+        return check(0,nums,visited);
+    }
+}
